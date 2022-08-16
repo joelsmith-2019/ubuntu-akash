2b82f72830e674cc71f0425855e7ed6fcaeb62c5
Joel Smith <joelsmith.2019@gmail.com>
2022-08-16 12:24:46 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-echo "Trying To Allow Root Login"
+source $HOME/.bashrc
+TZ=US/Arizona
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+apt-get update
+apt-get upgrade -y
+apt-get install -y sudo nano wget tar zip unzip jq goxkcdpwgen ssh nginx build-essential git make gcc nvme-cli pkg-config libssl-dev libleveldb-dev clang bsdmainutils ncdu libleveldb-dev apt-transport-https gnupg2
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-echo "My Cool Root Password = "${my_root_password}
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
-sleep 10
-systemctl restart sshd
-systemctl start nginx
+service ssh restart
+service nginx start
+sleep 5
+sudo apt-get install -y nano runit
+runsvdir -P /etc/service &
+source $HOME/.bashrc
