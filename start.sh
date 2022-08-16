3fb421af15574de847adea5990fd5b50ec64aafb
Joel Smith <joelsmith.2019@gmail.com>
2022-08-16 11:51:30 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "My Root Password = " ${my_root_password}
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
 sleep 5
 systemctl restart sshd
