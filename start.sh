0ff5632c1153c964e83fac44796ca1245f7c63b4
Joel Smith <joelsmith.2019@gmail.com>
2022-08-16 11:50:24 -0700

    Updates

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-source $HOME/.bashrc
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
 sleep 5
 systemctl restart sshd
 systemctl start nginx
-sleep 5
-pacman -Su nano
-runsvdir -P /etc/service &
-source $HOME/.bashrc
 
