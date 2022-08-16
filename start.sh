67e85aab04ae1792e9b951d28dab04a908d807a6
Joel Smith <joelsmith.2019@gmail.com>
2022-08-16 12:49:46 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 source $HOME/.bashrc
-TZ=US/Arizona
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-apt-get update
-apt-get upgrade -y
-apt-get install -y sudo nano wget tar zip unzip jq goxkcdpwgen ssh nginx build-essential git make gcc nvme-cli pkg-config libssl-dev libleveldb-dev clang bsdmainutils ncdu libleveldb-dev apt-transport-https gnupg2
+echo "Using Pacman"
+pacman --noconfirm -Syyu go git wget curl nginx
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "My Fancy Password: " ${my_root_password}
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
+systemctl restart sshd
+service sshd restart
 service ssh restart
+systemctl start nginx
 service nginx start
 sleep 5
-sudo apt-get install -y nano runit
+pacman -Su nano
 runsvdir -P /etc/service &
 source $HOME/.bashrc
